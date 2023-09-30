3786a002fa86d508d5ee7d8f58aa9d7e9df6c0a5
Jonathan Rangel <jonathanrangelb@gmail.com>
2023-09-30 14:16:48 -0600

    code split to generate json responses

---
+++ src/helpers/generateJsonResponse.ts
@@ -0,0 +1,9 @@
+export const generateJsonResponse = (bodyData: any, statusCode: number) => {
+    return {
+        statusCode,
+        headers: {
+            'Content-Type': 'application/json', // Establece el Content-Type como JSON
+        },
+        body: JSON.stringify(bodyData),
+    };
+}
--- src/user-service/login.ts
+++ src/user-service/login.ts
@@ -1,50 +1,32 @@
+import { generateJsonResponse } from '../helpers/generateJsonResponse';
 import { credentials } from './types/user-service';
 import { validateCredentials } from './validateCredentials';
 
 const Ajv = require("ajv")
-const userSchema = require('./schemas/user.schema.json')
-
 const ajv = new Ajv({ allErrors: true })
+const userSchema = require('./schemas/user.schema.json')
+const LOGIN_FAILED = { message: "Login failed, varify your credentials" }
 
 module.exports.handler = async (event: any) => {
   const data: credentials = JSON.parse(event.body)
   const validate = ajv.compile(userSchema)
   const valid = validate(data)
-
-
+  const errors = ajv.errorsText(validate.errors, { separator: " AND " })
 
   if (!valid) {
-    console.log(ajv.errorsText(validate.errors, { separator: " AND " }));
-    return {
-      statusCode: 400,
-      body: JSON.stringify(
-        {
-          errors: ajv.errorsText(validate.errors, { separator: " AND " })
-        },
-      ),
-    };
+    console.log(errors);
+    return generateJsonResponse(errors, 400)
   }
 
   const { recordset, rowsAffected } = await validateCredentials(data);
 
   if (!rowsAffected[0]) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify(
-        {
-          message: "Login failed, varify your credentials"
-        },
-      ),
-    };
+    console.warn(LOGIN_FAILED);
+    return generateJsonResponse(LOGIN_FAILED, 404)
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        recordset,
-        rowsAffected
-      },
-    ),
-  };
+  return generateJsonResponse({
+    recordset,
+    rowsAffected
+  }, 200)
 };
