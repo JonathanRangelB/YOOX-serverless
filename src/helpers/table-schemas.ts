b842396d3298216ca56eb312d25b9fe615aa83f7
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-09-11 18:19:29 -0600

    feat: agregando eslint y nuevas tablas
    se agrego eslint para apoyar con el desarollo y las sugerencias de
    codigo, tambien se agrego una nueva tabla al archivo de esquemas de
    tablas

--- src/helpers/table-schemas.ts
+++ src/helpers/table-schemas.ts
@@ -4,3 +4,39 @@ export interface pagos_reglas_dias {
   numero_dias: number;
 }
 
+export interface customer_loan_request {
+  ID_LOAN_REQUEST: number;
+  ID_CLIENT_REQUEST: number;
+  ID_CLIENT: number;
+}
+
+export interface customer_request {
+  ID: number;
+  NOMBRE: string;
+  TELEFONO_FIJO: string;
+  TELEFONO_MOVIL: string;
+  CORREO_ELECTRONICO: string;
+  ESTATUS: string;
+  OBSERVACIONES: string;
+  ID_AGENTE: number;
+  OCUPACION: string;
+  CURP: string;
+  ID_DOMICILIO: number;
+  TIPO_CALLE: string;
+  NOMBRE_CALLE: string;
+  NUMERO_EXTERIOR: string;
+  NUMERO_INTERIOR: string;
+  COLONIA: string;
+  MUNICIPIO: string;
+  ESTADO: string;
+  CP: string;
+  REFERENCIAS: string;
+  CREATED_BY: number;
+  CREATED_DATE: Date;
+  MODIFIED_BY: number;
+  MODIFIED_DATE: Date;
+  APPROVED_BY: number;
+  APPROVED_DATE: Date;
+  STATUS_CODE: number;
+  CONSTRAINT: number;
+}
--- src/new-requests/loan.ts
+++ src/new-requests/loan.ts
@@ -2,7 +2,7 @@ import { APIGatewayEvent } from "aws-lambda";
 import { generateJsonResponse } from "../helpers/generateJsonResponse";
 
 module.exports.handler = async (event: APIGatewayEvent) => {
-  let statusCode = 200;
+  let statusCode = 400;
   return generateJsonResponse(
     { message: "hola mundo desde loan-request" },
     statusCode,
