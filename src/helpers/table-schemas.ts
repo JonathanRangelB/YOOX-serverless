2dc64a281f1d301f475969611707ba3271f5e02e
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-09-11 18:57:56 -0600

    feat: nuevas interfases de tablas
    se crearon las nuevas interfases necesarias para el registro de
    prospectos a nuevos clientes y solicitudes de prestamos, tambien se hizo
    un pequeño cambio de un require a import

--- src/helpers/table-schemas.ts
+++ src/helpers/table-schemas.ts
@@ -38,5 +38,33 @@ export interface customer_request {
   APPROVED_BY: number;
   APPROVED_DATE: Date;
   STATUS_CODE: number;
-  CONSTRAINT: number;
+}
+
+export interface error_code {
+  ID: number;
+  STATUS_CODE: number;
+  MESSAGE: string;
+}
+
+export interface loan_request {
+  ID: number;
+  REQUEST_NUMBER: string;
+  ID_CLIENTE: number;
+  ID_PLAZO: number;
+  CANTIDAD_PRESTADA: number;
+  DIA_SEMANA: string;
+  FECHA_INICIAL: Date;
+  FECHA_FINAL_ESTIMADA: Date;
+  ID_COBRADOR: number;
+  CANTIDAD_PAGAR: number;
+  STATUS: string;
+  TASA_INTERES: number;
+  ID_GRUPO_ORIGINAL: number;
+  CREATED_BY: number;
+  CREATED_DATE: Date;
+  MODIFIED_BY: number;
+  MODIFIED_DATE: Date;
+  APPROVED_BY: number;
+  APPROVED_DATE: Date;
+  STATUS_CODE: number;
 }
--- src/user-service/login.ts
+++ src/user-service/login.ts
@@ -1,18 +1,18 @@
-import { generateJsonResponse } from '../helpers/generateJsonResponse';
-import { credentials } from './types/user-service';
-import { validateCredentials } from './validateCredentials';
+import { generateJsonResponse } from "../helpers/generateJsonResponse";
+import { credentials } from "./types/user-service";
+import { validateCredentials } from "./validateCredentials";
+import Ajv from "ajv";
 
-const jwt = require('jsonwebtoken');
-const Ajv = require('ajv');
+const jwt = require("jsonwebtoken");
 const ajv = new Ajv({ allErrors: true });
-const userSchema = require('./schemas/user.schema.json');
-const LOGIN_FAILED = { message: 'Login failed, verify your credentials' };
+const userSchema = require("./schemas/user.schema.json");
+const LOGIN_FAILED = { message: "Login failed, verify your credentials" };
 
 module.exports.handler = async (event: any) => {
   const data: credentials = JSON.parse(event.body);
   const validate = ajv.compile(userSchema);
   const valid = validate(data);
-  const errors = ajv.errorsText(validate.errors, { separator: ' AND ' });
+  const errors = ajv.errorsText(validate.errors, { separator: " AND " });
 
   if (!valid) {
     return generateJsonResponse(errors, 400);
@@ -27,7 +27,7 @@ module.exports.handler = async (event: any) => {
   }
 
   const token = jwt.sign({ recordset }, process.env.TOKEN_JWT, {
-    expiresIn: '30m',
+    expiresIn: "30m",
   });
 
   return generateJsonResponse(
@@ -35,6 +35,6 @@ module.exports.handler = async (event: any) => {
       user: recordset[0],
       Autorization: `Bearer ${token}`,
     },
-    200
+    200,
   );
 };
