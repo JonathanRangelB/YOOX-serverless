f108e828ff7fb293add051c4b7686b3dfa2db353
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-09-23 17:30:23 -0600

    chore: correccion de esquema para ajv

--- src/new-requests/loans/validateLoanData.ts
+++ src/new-requests/loans/validateLoanData.ts
@@ -1,23 +1,24 @@
-import { SPInsertNewLoanRequest } from "../types/SPInsertNewLoanRequest";
 import Ajv from "ajv";
+import formats from "ajv-formats";
+
+import { loanSchema } from "../schemas/loan.schema";
+import { SPInsertNewLoanRequest } from "../types/SPInsertNewLoanRequest";
 import { validateLoanResponse } from "./validateLoanResponse";
 
-//const loanSchema = require("../schemas/loan.schema.json")
-import { loanSchema } from "../schemas/loan.schema";
 const ajv = new Ajv({ allErrors: true });
+formats(ajv);
 
+export function isValidLoanData(
+  spInsertNewLoanRequest: SPInsertNewLoanRequest,
+): validateLoanResponse {
+  const validate = ajv.compile(loanSchema);
+  const valid = validate(spInsertNewLoanRequest);
+  const errors = ajv.errorsText(validate.errors, { separator: " AND " });
 
-export function isValidLoanData(spInsertNewLoanRequest : SPInsertNewLoanRequest) : validateLoanResponse {
+  if (!valid) {
+    return { valid: false, errors };
+  }
 
-    const validate = ajv.compile(loanSchema);
-    const valid = validate(spInsertNewLoanRequest);
-    const errors = ajv.errorsText(validate.errors, { separator: " AND " });
-
-
-    if (!valid) {
-        return {valid : false, errors}
-      }
-
-    return { valid : true }
-
+  return { valid: true };
 }
+
--- src/new-requests/schemas/loan.schema.ts
+++ src/new-requests/schemas/loan.schema.ts
@@ -1,30 +1,26 @@
 export const loanSchema = {
-  "type": "object",
-  "properties": {
-    "id": { "type":"integer"},
-    "request_number": { "type":"string"},
-    "id_cliente": { "type":"integer"},
-    "id_plazo": { "type":"integer"},
-    "cantidad_prestada": { "type":"number"},
-    "dia_semana": { "type":"string"},
-    "fecha_inicial": { "type":"string","format": "date"},
-    "fecha_final_estimada": { "type":"string","format": "date"},
-    "id_cobrador": { "type":"integer"},
-    "cantidad_pagar": { "type":"number"},
-    "status": { "type":"string"},
-    "tasa_integereres": { "type":"integer"},
-    "id_grupo_original": { "type":"integer"},
-    "created_by": { "type":"integer"},
-    "created_date": { "type":"string","format": "date"},
-    "modified_by": { "type":"integer"},
-    "modified_date": { "type":"string","format": "date"},
-    "approved_date": { "type":"string","format": "date"},
-    "approved_by": { "type":"integer"},
-    "status_code": { "type":"integer"}     
+  properties: {
+    id: { type: "integer" },
+    request_number: { type: "string" },
+    id_cliente: { type: "integer" },
+    id_plazo: { type: "integer" },
+    cantidad_prestada: { type: "number" },
+    dia_semana: { type: "string" },
+    fecha_inicial: { type: "string", format: "date-time" },
+    fecha_final_estimada: { type: "string", format: "date-time" },
+    id_cobrador: { type: "integer" },
+    cantidad_pagar: { type: "number" },
+    status: { type: "string" },
+    tasa_integereres: { type: "integer" },
+    id_grupo_original: { type: "integer" },
+    created_by: { type: "integer" },
+    created_date: { type: "string", format: "date" },
+    modified_by: { type: "integer" },
+    modified_date: { type: "string", format: "date" },
+    approved_date: { type: "string", format: "date" },
+    approved_by: { type: "integer" },
+    status_code: { type: "integer" },
   },
-  "required": ["id", "nombre", "status", "curp"]
-}
+  required: ["id", "nombre", "status"],
+};
 
-
-
-  
