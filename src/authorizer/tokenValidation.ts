be9a3df9d2951b10393760b58efc27aab9939179
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-09-22 21:18:30 -0600

    chore: corrigiendo mensaje de autorizacion de AWS
    incluyendo tambien el comando al package.json para la generacion de
    swagger de manera offline

--- src/authorizer/tokenValidation.ts
+++ src/authorizer/tokenValidation.ts
@@ -1,28 +1,29 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 import {
   JsonWebTokenError,
   TokenExpiredError,
   NotBeforeError,
-} from 'jsonwebtoken';
+} from "jsonwebtoken";
 
-import { generateJsonResponse } from '../helpers/generateJsonResponse';
+import { generateJsonResponse } from "../helpers/generateJsonResponse";
 
 module.exports.handler = async (event: any) => {
   const { headers } = event;
-  const authToken = headers?.authorization?.split(' ')[1];
+  const authToken = headers?.authorization?.split(" ")[1];
 
   try {
-    var decoded = jwt.verify(authToken, process.env.TOKEN_JWT);
+    jwt.verify(authToken, process.env.TOKEN_JWT);
     return generateJsonResponse({ isValid: true }, 200);
   } catch (err) {
+    console.warn("algo paso");
     if (err instanceof JsonWebTokenError) {
-      return generateJsonResponse({ isValid: false, error: err.message }, 401);
+      return generateJsonResponse({ isValid: false, error: err }, 401);
     }
     if (err instanceof TokenExpiredError) {
-      return generateJsonResponse({ isValid: false, error: err.message }, 400);
+      return generateJsonResponse({ isValid: false, error: err }, 400);
     }
     if (err instanceof NotBeforeError) {
-      return generateJsonResponse({ isValid: false, error: err.message }, 400);
+      return generateJsonResponse({ isValid: false, error: err }, 400);
     }
     return generateJsonResponse({ isValid: false, error: err }, 500);
   }
