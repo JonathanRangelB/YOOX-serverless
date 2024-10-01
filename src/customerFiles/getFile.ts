71674c26c0c0f9f21e580214b87280d1d791278d
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-09-30 18:13:54 -0600

    feat: agregando endpoint generador de URLs firmadas para S3

--- src/customerFiles/getFile.ts
+++ src/customerFiles/getFile.ts
@@ -2,5 +2,5 @@ import { APIGatewayEvent } from "aws-lambda";
 import { generateJsonResponse } from "../helpers/generateJsonResponse";
 
 module.exports.handler = async (event: APIGatewayEvent) => {
-  return generateJsonResponse({ message: "holawa desde upload" }, 200);
+  return generateJsonResponse({ message: "holawa desde getFile" }, 200);
 };
---
+++ src/customerFiles/uploadFiles.ts
@@ -0,0 +1,43 @@
+import { APIGatewayEvent } from "aws-lambda";
+import { S3 } from "aws-sdk";
+
+import { generateJsonResponse } from "../helpers/generateJsonResponse";
+
+const s3 = new S3();
+
+module.exports.handler = async (event: APIGatewayEvent) => {
+  if (!event.body)
+    return generateJsonResponse({ message: "No body provided" }, 400);
+
+  const bucketName = process.env.BUCKET_NAME || "documentos-clientes-yoox";
+  const { filenames } = JSON.parse(event.body) as { filenames: string[] };
+
+  if (filenames.length > 10)
+    return generateJsonResponse(
+      { message: "Solo se permite un maximo de 10 archivos" },
+      400,
+    );
+
+  try {
+    const uploadUrls = await Promise.all(
+      filenames.map(async (filename: string) => {
+        const params = {
+          Bucket: bucketName,
+          Key: `temporal/${filename}`, // Archivo que se subirá con su ruta completa. Ej: documentos/imagen/{filename}
+          Expires: 30, // La URL será válida por 30 segundos
+          ContentType: "application/octet-stream", // Tipo de contenido generico binario, osea cualquier tipo de archivo
+        };
+        const signedUrl = await s3.getSignedUrlPromise("putObject", params);
+        return { filename, signedUrl };
+      }),
+    );
+
+    return generateJsonResponse({ uploadUrls }, 200);
+  } catch (error) {
+    if (error instanceof Error)
+      return generateJsonResponse(
+        { message: "Error generating signed URL", error: error.message },
+        500,
+      );
+  }
+};
