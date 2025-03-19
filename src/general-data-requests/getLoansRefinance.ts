02e24a5ddced976b99b82809b1975d0092e32e54
oberrones <oneiros86@gmail.com>
2025-03-19 17:14:26 -0600

    chore: ejecucion de pnpm lint

--- src/general-data-requests/getLoansRefinance.ts
+++ src/general-data-requests/getLoansRefinance.ts
@@ -6,13 +6,13 @@ import { loan_refinance } from '../helpers/table-schemas';
 import { generateJsonResponse } from '../helpers/generateJsonResponse';
 
 module.exports.handler = async (event: APIGatewayEvent) => {
-    try {
-        const customerid = event.queryStringParameters?.customerid;
+  try {
+    const customerid = event.queryStringParameters?.customerid;
 
-        if (!customerid || isNaN(+customerid) || +customerid <= 0)
-            throw new Error('Parametros incompletos');
-        const pool = await DbConnector.getInstance().connection;
-        const query = `
+    if (!customerid || isNaN(+customerid) || +customerid <= 0)
+      throw new Error('Parametros incompletos');
+    const pool = await DbConnector.getInstance().connection;
+    const query = `
                 select
                 t0.id as id_prestamo,
                 t0.id_cliente,
@@ -39,15 +39,15 @@ module.exports.handler = async (event: APIGatewayEvent) => {
                 and t1.num_de_pagos = 10
                 and t0.id_cliente = ${customerid} `;
 
-        const result = await pool.query<loan_refinance>(query);
+    const result = await pool.query<loan_refinance>(query);
 
-        if (result.recordset.length == 0)
-            throw new Error('No se encontraron registros');
+    if (result.recordset.length == 0)
+      throw new Error('No se encontraron registros');
 
-        return generateGetJsonResponse(result.recordsets[0], StatusCodes.OK);
-    } catch (err) {
-        if (err instanceof Error) {
-            return generateJsonResponse(err.message, StatusCodes.BAD_REQUEST);
-        }
+    return generateGetJsonResponse(result.recordsets[0], StatusCodes.OK);
+  } catch (err) {
+    if (err instanceof Error) {
+      return generateJsonResponse(err.message, StatusCodes.BAD_REQUEST);
     }
+  }
 };
