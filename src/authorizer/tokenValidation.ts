722c5990a81ac96a1f6a262209b6673e4bc83609
Jonathan Rangel <jonathanrangelb@gmail.com>
2025-05-31 22:55:27 -0600

    chore: mejoras menores en tokenValidation

--- src/authorizer/tokenValidation.ts
+++ src/authorizer/tokenValidation.ts
@@ -6,6 +6,7 @@ import {
 } from 'jsonwebtoken';
 
 import { generateJsonResponse } from '../helpers/generateJsonResponse';
+import { StatusCodes } from '../helpers/statusCodes';
 
 module.exports.handler = async (event: any) => {
   const { headers } = event;
@@ -13,17 +14,29 @@ module.exports.handler = async (event: any) => {
 
   try {
     jwt.verify(authToken, process.env.TOKEN_JWT!);
-    return generateJsonResponse({ isValid: true }, 200);
+    return generateJsonResponse({ isValid: true }, StatusCodes.OK);
   } catch (err) {
     if (err instanceof JsonWebTokenError) {
-      return generateJsonResponse({ isValid: false, error: err }, 401);
+      return generateJsonResponse(
+        { isValid: false, error: err },
+        StatusCodes.UNAUTHORIZED
+      );
     }
     if (err instanceof TokenExpiredError) {
-      return generateJsonResponse({ isValid: false, error: err }, 400);
+      return generateJsonResponse(
+        { isValid: false, error: err },
+        StatusCodes.BAD_REQUEST
+      );
     }
     if (err instanceof NotBeforeError) {
-      return generateJsonResponse({ isValid: false, error: err }, 400);
+      return generateJsonResponse(
+        { isValid: false, error: err },
+        StatusCodes.BAD_REQUEST
+      );
     }
-    return generateJsonResponse({ isValid: false, error: err }, 500);
+    return generateJsonResponse(
+      { isValid: false, error: err },
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
   }
 };
