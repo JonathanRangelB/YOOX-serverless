775846a5fc1e3cf67fa7bde13161390f20343223
Jonathan Rangel <jonathanrangelb@gmail.com>
2025-06-26 11:26:03 -0600

    chore: code cleanup

--- src/whatsapp/sqsHandler.ts
+++ src/whatsapp/sqsHandler.ts
@@ -1,37 +1,10 @@
 import { SQSEvent } from 'aws-lambda';
 import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
-import {
-  SimpleTextMessage,
-  TemplateMessage,
-  WhatsAppMessage,
-} from './interfaces/whatsappMessage';
+
+import { WhatsAppMessage } from './interfaces/whatsappMessage';
 import { transformSQSRecordAttributes } from './utils/transformMessageAttributes';
-
-const processSimpleTextMessage = async (message: SimpleTextMessage) => {
-  const result = await fetch('http://localhost:3001/api/sendText', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      chatId: `${message.to}@c.us`,
-      text: message.body,
-      session: 'default',
-    }),
-  });
-  if (!result) {
-    // TODO: guardar el error en la base de datos, puede deberse a que la sesion "default" no existe o que no este "corriendo"
-    // si se da el segundo caso seria ejecutar un POST a http://localhost:3001/api/sessions/default/start (o a la URL que corresponda) para iniciar la sesion "default"
-    // si no existe la sesion "default" se debe crear con una peticion POST a '/api/sessions' (las instruciones estan en https://github.com/devlikeapro/waha?tab=readme-ov-file)
-    // despues ya solo solicitas el QR con una peticion GET a '/api/:session/auth/qr?format=image'
-  }
-  console.log(result);
-};
-
-// Función para procesar un mensaje de plantilla
-const processTemplateMessage = (message: TemplateMessage) => {
-  console.log(`Enviando plantilla "${message.templateName}" a ${message.to}`);
-};
+import { processSimpleTextMessage } from './simpleTextHandler';
+import { processTemplateMessage } from './templateHandler';
 
 export const handler = async (event: SQSEvent) => {
   for (const record of event.Records) {
