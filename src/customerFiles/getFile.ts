f6126668e1e813506792385fa6e20cad9a853d82
Jonathan Rangel <jonathanrangelb@gmail.com>
2025-09-08 11:28:15 -0600

    fix: agregando encoding UTF-8
    corrigel el problema con los caracteres especiales en los nombres de
    archivo, por ejemplo palabras acentuadas

--- src/customerFiles/getFile.ts
+++ src/customerFiles/getFile.ts
@@ -1,16 +1,16 @@
-import { APIGatewayEvent } from 'aws-lambda';
+import { APIGatewayEvent } from "aws-lambda";
 import {
   S3Client,
   GetObjectCommand,
   GetObjectRequest,
-} from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-import { generateJsonResponse } from '../helpers/generateJsonResponse';
-import { StatusCodes } from '../helpers/statusCodes';
-import { FileDataSchema } from './schemas/file.schema';
-import { FileData } from './types/fileData';
-import { validatePayload } from '../helpers/utils';
+import { generateJsonResponse } from "../helpers/generateJsonResponse";
+import { StatusCodes } from "../helpers/statusCodes";
+import { FileDataSchema } from "./schemas/file.schema";
+import { FileData } from "./types/fileData";
+import { validatePayload } from "../helpers/utils";
 
 const client = new S3Client({
   region: process.env.MY_AWS_REGION,
@@ -23,8 +23,8 @@ const client = new S3Client({
 module.exports.handler = async (event: APIGatewayEvent) => {
   if (!event.body)
     return generateJsonResponse(
-      { message: 'No body provided' },
-      StatusCodes.BAD_REQUEST
+      { message: "No body provided" },
+      StatusCodes.BAD_REQUEST,
     );
 
   const data: FileData = JSON.parse(event.body);
@@ -33,21 +33,21 @@ module.exports.handler = async (event: APIGatewayEvent) => {
   if (!validData.valid)
     return generateJsonResponse(
       {
-        message: 'Object provided invalid',
+        message: "Object provided invalid",
         error: validData.error,
         additionalProperties: validData.additionalProperties,
       },
-      StatusCodes.BAD_REQUEST
+      StatusCodes.BAD_REQUEST,
     );
 
   const { filename, path } = data;
 
-  const bucketName = process.env.BUCKET_NAME || 'documentos-clientes-yoox';
+  const bucketName = process.env.BUCKET_NAME || "documentos-clientes-yoox";
   const upperCasePath = path.toUpperCase();
   const params: GetObjectRequest = {
     Bucket: bucketName,
     Key: upperCasePath + filename,
-    ResponseContentDisposition: `attachment; filename="${filename}"`,
+    ResponseContentDisposition: `attachment; filename*=UTF-8''${encodeURIComponent(filename)}`,
   };
 
   try {
@@ -58,7 +58,7 @@ module.exports.handler = async (event: APIGatewayEvent) => {
     if (error instanceof Error)
       return generateJsonResponse(
         { error: error.message },
-        StatusCodes.INTERNAL_SERVER_ERROR
+        StatusCodes.INTERNAL_SERVER_ERROR,
       );
   }
 };
