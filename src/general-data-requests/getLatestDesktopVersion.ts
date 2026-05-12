1e635eaec6b19f4307d6b4e91fd02a5ce18753dc
Jonathan Rangel Bernal <jonathanrangelb@gmail.com>
2026-05-12 00:48:54 -0600

    fix: corrigiendo las auth
    habian un par de cambios que se debian de aplicar:
    habilitar el enableSimpleResponses
    agregar la ruta de la app desplegada en vercel
    cambiar el codigo del authorizer de un callback por async, lo cual es
    mandatorio para la version 24 en adelante de node, ahora el codigo es
    mas claro y simple gracias a este cambio de node

--- src/general-data-requests/getLatestDesktopVersion.ts
+++ src/general-data-requests/getLatestDesktopVersion.ts
@@ -1,24 +1,30 @@
 import { APIGatewayEvent } from "aws-lambda";
+import { DbConnector } from "../helpers/dbConnector";
+import { generateGetJsonResponse } from "../helpers/generateGetJsonResponse";
 import { generateJsonResponse } from "../helpers/generateJsonResponse";
 import { StatusCodes } from "../helpers/statusCodes";
-import { DbConnector } from "../helpers/dbConnector";
 import { DesktopVersionData } from "./types/desktopVersionData";
-import { generateGetJsonResponse } from "../helpers/generateGetJsonResponse";
 
 module.exports.handler = async (_: APIGatewayEvent) => {
   try {
+    console.log("Iniciando handler");
     const con = await DbConnector.getInstance().connection;
+    console.log("Conexion obtenida");
     const res = await con.query<DesktopVersionData>(
       "SELECT TOP 1 [PUBLISH_URL] as [url] FROM SEC_VERSION sv ORDER BY sv.ID_VERSION DESC"
     );
+    console.log("Query ejecutado", JSON.stringify(res.recordset));
     if (res.recordset.length === 0) {
       return generateJsonResponse(
         { error: "Query throw no data" },
         StatusCodes.NOT_FOUND
       );
     }
-    return generateGetJsonResponse(res.recordset[0], StatusCodes.OK);
+    const response = generateGetJsonResponse(res.recordset[0], StatusCodes.OK);
+    console.log("Respuesta generada", JSON.stringify(response));
+    return response;
   } catch (error) {
+    console.error("Error capturado", JSON.stringify(error));
     return generateJsonResponse(error, StatusCodes.INTERNAL_SERVER_ERROR);
   }
 };
--- src/helpers/generateGetJsonResponse.ts
+++ src/helpers/generateGetJsonResponse.ts
@@ -6,13 +6,12 @@ export const generateGetJsonResponse = (
 ) => {
   const oneMinuteInSeconds = 60;
   const expires = new Date(new Date().getTime() + oneMinuteInSeconds * 1000);
-
   return {
     statusCode,
     headers: {
       "Content-Type": "application/json",
-      "Cache-Control": `public, must-revalidate, max-age=${oneMinuteInSeconds}`, // Controla la duración del caché
-      Expires: expires, // Fecha exacta de expiración
+      "Cache-Control": `public, must-revalidate, max-age=${oneMinuteInSeconds}`,
+      Expires: expires.toUTCString(),
     },
     body: JSON.stringify(bodyData),
   };
